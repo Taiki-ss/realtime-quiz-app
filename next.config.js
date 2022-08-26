e01979e634b7bac164f3fa139e9b0a9963c80f25
Taiki-ss <taiki.ishida.ss@gmail.com>
2022-08-26 10:55:46 +0900

    :bug:[fix] exportPath削除してみた

--- next.config.js
+++ next.config.js
@@ -6,14 +6,5 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      "/": { page: "/" },
-      "/quiz": { page: "/quiz" },
-    };
-  },
   trailingSlash: true,
 };
