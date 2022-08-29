6bc9a10af10e2a23fab68663a797dce73b67bc62
Taiki-ss <taiki.ishida.ss@gmail.com>
2022-08-29 17:58:39 +0900

    :poop:[save] 相変わらずクソコードだった

--- pages/result/index.js
+++ pages/result/index.js
@@ -17,6 +17,13 @@ export default function Result() {
   // }
   // },[time,showStatus])
 
+  const sleep = (time) => new Promise((resolve) => setTimeout(resolve, time));
+
+  const test = async () => {
+    await sleep(3000);
+    console.log("表示");
+  };
+
   useEffect(() => {
     axios
       .get("/api/result")
@@ -40,7 +47,7 @@ export default function Result() {
   const menberList = result.map((obj) => {
     return obj.member.map((mem) => {
       return (
-        <tr key={mem.name}>
+        <tr className="late" key={mem.count} data-count={mem.count}>
           <td>{obj.lank}位</td>
           <td>{mem.name}</td>
           <td>{mem.porto}</td>
@@ -50,6 +57,20 @@ export default function Result() {
       );
     });
   });
+
+  const showRanking = () => {
+	const length = document.querySelectorAll('.late').length
+	let time = 0;
+    for (let i = length-1; i >= 0; i--) {
+        setTimeout(() => {
+          console.log(document.querySelector(`.late[data-count="${i}"]`));
+          document.querySelector(`.late[data-count="${i}"]`).style.display =
+            "table-row";
+        }, 100 * time);
+		time++
+    }
+  };
+
   console.log(result);
 
   return (
@@ -61,6 +82,8 @@ export default function Result() {
       >
         <Link href={"/"}>TOPへ戻る</Link>
       </button>
+
+      <button onClick={showRanking}>表示</button>
       <main className={styles.main}>
         <h1 className={styles.title}>結果発表</h1>
         <p>{showStatus && time > 0 ? time + "秒" : ""}</p>
