6d228f3c714d62dc88eeacdbc0944906d25b76f7
Taiki-ss <taiki.ishida.ss@gmail.com>
2022-08-30 11:07:35 +0900

    :rocket:[feat] ランキングを順番に表示できるようにはなった

--- pages/result/index.js
+++ pages/result/index.js
@@ -6,6 +6,10 @@ import axios from "axios";
 export default function Result() {
   const [showStatus, setShowStatus] = useState(false);
   const [result, setResult] = useState([]);
+  const [showcount, setShowcount] = useState(0);
+  const [amariDelete, setAmariDelete] = useState(false);
+  const [top5, setTop5] = useState(false);
+  const [top5count, setTop5count] = useState(5);
   // const [time, setTime] = useState(10);
   const [time, setTime] = useState(0);
 
@@ -47,7 +51,11 @@ export default function Result() {
   const menberList = result.map((obj) => {
     return obj.member.map((mem) => {
       return (
-        <tr className="late" key={mem.count} data-count={mem.count}>
+        <tr
+          className={obj.lank === 1 ? "late rank1" : "late"}
+          key={mem.count}
+          data-count={mem.count}
+        >
           <td>{obj.lank}位</td>
           <td>{mem.name}</td>
           <td>{mem.porto}</td>
@@ -59,16 +67,75 @@ export default function Result() {
   });
 
   const showRanking = () => {
-	const length = document.querySelectorAll('.late').length
-	let time = 0;
-    for (let i = length-1; i >= 0; i--) {
+    document.querySelectorAll(".late").forEach((v) => {
+      v.style.display = "none";
+    });
+    const length = document.querySelectorAll(".late").length;
+    let kaisuu = Math.floor(length / 10);
+    let amari = length % 10;
+
+    if (document.querySelectorAll(".rank1").length + 2 > top5count) {
+      setTop5count(document.querySelectorAll(".rank1").length + 2);
+    }
+
+    console.log(kaisuu);
+    console.log(amari);
+    if (!amariDelete && amari !== 0) {
+      let time = 0;
+      for (let i = 10 * kaisuu + amari - 1; i >= 10 * kaisuu; i--) {
         setTimeout(() => {
-          console.log(document.querySelector(`.late[data-count="${i}"]`));
           document.querySelector(`.late[data-count="${i}"]`).style.display =
             "table-row";
         }, 100 * time);
-		time++
+        time++;
+      }
+      setAmariDelete(true);
+      return;
     }
+
+    if (kaisuu - showcount > 1) {
+      let time = 0;
+      for (
+        let i = 10 * (kaisuu - showcount) - 1;
+        i >= 10 * (kaisuu - showcount - 1);
+        i--
+      ) {
+        setTimeout(() => {
+          document.querySelector(`.late[data-count="${i}"]`).style.display =
+            "table-row";
+        }, 100 * time);
+        time++;
+      }
+      setShowcount(showcount++);
+      return;
+    }
+
+    if (kaisuu - showcount === 1) {
+      let time = 0;
+      for (
+        let i = 10 * (kaisuu - showcount) - 1;
+        i >= 10 * (kaisuu - showcount - 1) + top5count;
+        i--
+      ) {
+        setTimeout(() => {
+          document.querySelector(`.late[data-count="${i}"]`).style.display =
+            "table-row";
+        }, 100 * time);
+        time++;
+      }
+      setTop5(true);
+      return;
+    }
+  };
+
+  const showTop5 = () => {
+    document.querySelector(
+      `.late[data-count="${top5count - 1}"]`
+    ).style.backgroundColor = "pink";
+    document.querySelector(
+      `.late[data-count="${top5count - 1}"]`
+    ).style.display = "table-row";
+    if (top5count !== 1) setTop5count(top5count--);
   };
 
   console.log(result);
@@ -83,7 +150,8 @@ export default function Result() {
         <Link href={"/"}>TOPへ戻る</Link>
       </button>
 
-      <button onClick={showRanking}>表示</button>
+      <button onClick={showRanking}>ランキング表示</button>
+      <button onClick={showTop5}>TOP５くらい表示</button>
       <main className={styles.main}>
         <h1 className={styles.title}>結果発表</h1>
         <p>{showStatus && time > 0 ? time + "秒" : ""}</p>
