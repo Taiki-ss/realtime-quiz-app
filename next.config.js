6a8ad427c4b816a02f460dbcb8865213a4ea953c
Taiki-ss <taiki.ishida.ss@gmail.com>
2022-09-01 14:47:05 +0900

    :bug:[fix] パス調整

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = {
     includePaths: [path.join(__dirname, "styles")],
   },
   trailingSlash: true,
-  assetPrefix: "./",
+  assetPrefix: "/engineer-king/",
   async rewrites() {
     return [
       {
