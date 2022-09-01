f0a337fa2c8b474d83bda72928eda903358de253
Taiki-ss <taiki.ishida.ss@gmail.com>
2022-09-01 12:03:00 +0900

    :rocket:[fix] デプロイに向けていろいろ調整

--- next.config.js
+++ next.config.js
@@ -2,9 +2,19 @@
 const path = require("path");
 
 module.exports = {
+  basePath: "/engineer-king",
   pageExtensions: ["mdx", "md", "jsx", "js", "tsx", "ts"],
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
   trailingSlash: true,
+  assetPrefix: "/engineer-king/out",
+  async rewrites() {
+    return [
+      {
+        source: "/engineer-king/out/:path*",
+        destination: "/engineer-king/:path*",
+      },
+    ];
+  },
 };
